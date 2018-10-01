900e919da201cf9f34d7fa89f8ae812b8a47990a
susuwatary <11a66k@gmail.com>
2018-10-01 23:27:08 +0300

    create new test_add_contact

--- test_add_contact.py
+++ test_add_contact.py
@@ -16,9 +16,9 @@ class UntitledTestCase(unittest.TestCase):
     def test_untitled_test_case(self):
         wb = self.wd
         self.open_page(wb)
-        self.login(wb)
+        self.login(wb, usermane="admin", password="secret")
         self.create_new_contact(wb)
-        self.fill_contact_form(wb)
+        self.fill_contact_form(wb, first_name="rick", last_name="mortiy", address="USA", phone_home="+1000000", email="123@32.32")
         self.submin_form(wb)
         self.logout(wb)
 
@@ -28,32 +28,32 @@ class UntitledTestCase(unittest.TestCase):
     def submin_form(self, wb):
         wb.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
-    def fill_contact_form(self, wb):
+    def fill_contact_form(self, wb, first_name, last_name, address, phone_home, email):
         wb.find_element_by_name("firstname").click()
         wb.find_element_by_name("firstname").clear()
-        wb.find_element_by_name("firstname").send_keys("rick")
+        wb.find_element_by_name("firstname").send_keys(first_name)
         wb.find_element_by_name("lastname").click()
         wb.find_element_by_name("lastname").clear()
-        wb.find_element_by_name("lastname").send_keys("mortiy")
+        wb.find_element_by_name("lastname").send_keys(last_name)
         wb.find_element_by_name("address").click()
         wb.find_element_by_name("address").clear()
-        wb.find_element_by_name("address").send_keys("USA")
+        wb.find_element_by_name("address").send_keys(address)
         wb.find_element_by_name("home").click()
         wb.find_element_by_name("home").clear()
-        wb.find_element_by_name("home").send_keys("+1000000")
+        wb.find_element_by_name("home").send_keys(phone_home)
         wb.find_element_by_name("email").click()
         wb.find_element_by_name("email").clear()
-        wb.find_element_by_name("email").send_keys("123@32.32")
+        wb.find_element_by_name("email").send_keys(email)
 
     def create_new_contact(self, wb):
         wb.find_element_by_link_text("add new").click()
 
-    def login(self, wb):
-        wb.find_element_by_name("user").send_keys("admin")
+    def login(self, wb, usermane, password):
+        wb.find_element_by_name("user").send_keys(usermane)
         wb.find_element_by_xpath("//form[@id='LoginForm']/label[2]").click()
         wb.find_element_by_name("pass").click()
         wb.find_element_by_name("pass").clear()
-        wb.find_element_by_name("pass").send_keys("secret")
+        wb.find_element_by_name("pass").send_keys(password)
         wb.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_page(self, wb):
