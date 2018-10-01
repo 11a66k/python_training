71e3d0fea64eb4af00545f9c9ed3df88c842ea71
susuwatary <11a66k@gmail.com>
2018-10-01 22:34:38 +0300

    refactor mehod test_add_group

--- test_add_group.py
+++ test_add_group.py
@@ -13,16 +13,29 @@ class UntitledTestCase(unittest.TestCase):
         self.wd.implicitly_wait(30)
 
 
-    def test_untitled_test_case(self):
+    def test_add_group(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
         self.init_group_creation(wd)
-        self.fiil_group_form(wd)
+        self.fiil_group_form(wd, name="222", header="1111", footer="2222")
         self.sumbit_group_creation(wd)
         self.return_to_group_page(wd)
         self.logout(wd)
+
+
+    def test_case_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.init_group_creation(wd)
+        self.fiil_group_form(wd, name="", header="", footer="")
+        self.sumbit_group_creation(wd)
+        self.return_to_group_page(wd)
+        self.logout(wd)
+
 
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
@@ -33,17 +46,17 @@ class UntitledTestCase(unittest.TestCase):
     def sumbit_group_creation(self, wd):
         wd.find_element_by_name("submit").click()
 
-    def fiil_group_form(self, wd):
+    def fiil_group_form(self, wd, name, header, footer):
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("222")
+        wd.find_element_by_name("group_name").send_keys(name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("1111")
+        wd.find_element_by_name("group_header").send_keys(header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("2222")
+        wd.find_element_by_name("group_footer").send_keys(footer)
 
     def init_group_creation(self, wd):
         wd.find_element_by_name("new").click()
@@ -51,14 +64,14 @@ class UntitledTestCase(unittest.TestCase):
     def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
 
-    def login(self, wd):
+    def login(self, wd, username, password):
 
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_id("LoginForm").click()
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self, wd):
