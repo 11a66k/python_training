41440c1f126c2f7b59db8b92ffe23378b41175b5
susuwatary <11a66k@gmail.com>
2018-11-24 17:13:40 +0300

    Добавили конфигурационный файл

--- conftest.py
+++ conftest.py
@@ -1,21 +1,22 @@
 import pytest
 from fixture.application import Applicaion
-
+import json
 
 fixture = None
+target = None
 
 @pytest.fixture
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    if fixture is None:
-        fixture = Applicaion(browser=browser, base_url=base_url)
+    if target is None:
+        with open(request.config.getoption("--target")) as config_file:
+            target = json.load(config_file)
+    if fixture is None or not fixture.is_valid():
+        fixture = Applicaion(browser=browser, base_url=target['baseUrl'])
         # fixture.session.login(username="admin", password="secret")
-    else:
-        if not fixture.is_valid():
-            fixture = Applicaion(browser=browser, base_url=base_url)
-    fixture.session.ensure_login(username="admin", password="secret")
+    fixture.session.ensure_login(username=target["username"], password=target["password"])
     return fixture
 
 
@@ -29,4 +30,4 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/index.php")
+    parser.addoption("--target", action="store", default="target.json")
