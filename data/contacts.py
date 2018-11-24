34a74fadd950c7b0984e5ac18b137cc6e4de9165
susuwatary <11a66k@gmail.com>
2018-11-24 19:46:04 +0300

    jsonpickle -  не работает

---
+++ data/contacts.py
@@ -0,0 +1,12 @@
+from model.contact import Contact
+
+
+testdata = [
+    Contact(first_name="first_name1", last_name="last_name1", address="address1",
+            phone_home="phone_home1", phone_mobile="phone_mobile1", phone_work="phone_work1",
+            email="email1", email2="email21", email3="email31"),
+    Contact(first_name="first_name2", last_name="last_name2", address="address2",
+            phone_home="phone_home2", phone_mobile="phone_mobile2", phone_work="phone_work2",
+            email="email2", email2="email22", email3="email32")
+]
+
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 import pytest
 from model.contact import Contact
-from data.add_contact import testdata
 
 
 
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contact = app.contact.get_contact_list()
     app.contact.create_new(contact)
     new_contact = app.contact.get_contact_list()
